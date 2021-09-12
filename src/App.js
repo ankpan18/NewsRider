b3c654f17e4db6d01ae3f469e7b52bdaf589a3f3
ankpan18 <panthriankur@outlook.com>
2021-09-12 10:32:17 +0530

    Converted to Function Based Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import React, { Component } from 'react'
-import Navbar from './components/Navbar';
+import NavBar from './components/Navbar';
 import News from './components/News';
 import {
   BrowserRouter as Router,
@@ -10,62 +10,37 @@ import {
 } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-
-// console.log("asd",this.state.data)
 export default class App extends Component {
+  pageSize = 5;
+  apiKey = process.env.REACT_APP_NEWS_API
 
-pageSize=5;
-apiKey=process.env.REACT_APP_NEWS_API
-// q='';
-constructor(props){
-  super(props);
-  this.state={
-    q:""
+  state = {
+    progress:0
   }
-}
-// handledata(searchdata)
-// {
-//   console.log(searchdata)
-// }
-handledata(childData) {
-  this.setState({ q: childData });
-  // console.log(childData)
-  console.log("app.js q:",this.state.q)
-}
 
-state={
-  progress:0
-}
-
-setProgress=(progress)=>{
-  this.setState({
-    progress:progress,
-  })
-}
+  setProgress = (progress)=>{
+    this.setState({progress: progress})
+  }
 
   render() {
     return (
       <div>
         <Router>
-        <Navbar data={this.handledata.bind(this)} />
+        <NavBar/> 
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={this.state.progress}
+        progress={this.state.progress} 
       />
         <Switch>
-    
-    <Route exact key={this.state.q} path="/"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="home" pageSize={this.pageSize} country="in" category="general" q={this.state.q}/> </Route>
-    <Route exact key={this.state.q} path="/Search"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="Search" pageSize={this.pageSize} country="in" category="" q={this.state.q}/> </Route>
-    {/* <Route exact path="/{this.state.q}"><News setProgress={setProgress}  key="general" pageSize={this.pageSize} country="in" category="general" q={this.state.q} /> </Route> */}
-    {/* <Route exact path="/"><News setProgress={setProgress}  key="general" pageSize={this.pageSize} country="in" category="general" q="covid"/> </Route> */}
-    <Route exact key={this.state.q} path="/business"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="business" pageSize={this.pageSize} country="in" category="business"  q={this.state.q}/> </Route>
-    <Route exact key={this.state.q} path="/entertainment"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" q={this.state.q}/> </Route>
-    <Route exact key={this.state.q} path="/general"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="general" pageSize={this.pageSize} country="in" category="general" q={this.state.q}/> </Route>
-    <Route exact key={this.state.q} path="/health"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="health" pageSize={this.pageSize} country="in" category="health" q={this.state.q}/> </Route>
-    <Route exact key={this.state.q} path="/science"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="science" pageSize={this.pageSize} country="in" category="science" q={this.state.q}/> </Route>
-    <Route exact key={this.state.q} path="/sports"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="sports" pageSize={this.pageSize} country="in" category="sports" q={this.state.q}/> </Route>
-    <Route exact key={this.state.q} path="/technology"><News setProgress={this.setProgress} apiKey={this.apiKey}  key="technology" pageSize={this.pageSize} country="in" category="technology" q={this.state.q}/> </Route>
+          <Route exact path="/"><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/business"><News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business"/></Route> 
+          <Route exact path="/entertainment"><News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route> 
+          <Route exact path="/general"><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/health"><News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health"/></Route> 
+          <Route exact path="/science"><News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science"/></Route> 
+          <Route exact path="/sports"><News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route> 
+          <Route exact path="/technology"><News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route> 
         </Switch>
         </Router>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,114 +1,35 @@
-import React, { Component } from "react";
-import { Link,} from "react-router-dom";
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
-export class Navbar extends Component {
-  constructor(){
-    super();
-    this.state={
-      data:''
-    }
-  }
+const NavBar = () => {
 
-  // handleSearch(event){
-  //   // this.setState({
-  //   //   data:event.target.value,
-  //   // })
-  //   this.props.data(event.target.value);
-  //   console.log("hi:",event.target.value)
-    
-  // }
-  // onChange = event => {
-  //   this.props.data(event.target.value);
-  //   this.setState({
-  //       data:event.target.value,
-  //     })
-  // };
-
-  handleSearch=async(event)=> {
-         console.log("Search is working");
-     this.setState({
-       data: event.target.value
-      
-     })
-     this.props.data(event.target.value)
-     console.log(this.state.data);
-   }
-
-  render() {
-  //  let  q  =
-  //      this.props;
-       
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              NewsMonkey
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/">
-                    Home
-                  </Link>
-                </li>
-               
-                <li className="nav-item">
-                  <Link className="nav-link" to="/business">Business</Link></li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/general">General</Link></li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/health">Health</Link></li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/science">Science</Link></li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sports">Sports</Link></li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/technology">Technology</Link></li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Search">Search</Link></li>
-              </ul>
-              <form className="d-flex">
-        <input className="form-control me-2" type="search" onChange={this.handleSearch.bind(this)} placeholder="Search" aria-label="Search"/>
-        {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
-        {/* <button className="btn btn-outline-success" type="submit"><Link className="nav-link" to={`/search/${this.state.data}`}>Search</Link></button> */}
-        {/* <button className="btn btn-outline-success" type="submit"> */}
-        {/* <Switch>
-          <Route exact path="/search">
-            <News/>
-          </Route>
-        </Switch> */}
-          <Link className="nav-link" to="/Search">Search</Link>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 
-          {/* search */}
-          {/* </button> */}
-       
-      </form>
-            </div>
-          </div>
-        </nav>
-        <h2>Search entry:{this.state.data}</h2>
-        {console.log(this.state.data)}
-       
-      </div>
-    );
-  }
 }
 
-export default Navbar;
+export default NavBar
